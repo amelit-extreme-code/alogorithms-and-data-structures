d8d6452e0b13edfbb2cd8f4a849ddb1ffac9e27b
Amelit <muradovt111@gmail.com>
2021-09-06 21:13:47 +0300

    linear-search

---
+++ linear-search.js
@@ -0,0 +1,19 @@
+﻿const arr = [3,1,2,6,7,9,7,11,34,567,77,555,33,223]
+//TODO: refactor this function
+
+function linearSearch(value) {
+    for (let i = 0; i <= arr.length; i++) {
+        let returnedArr = []
+        if (arr[i] === value) {
+            console.log(arr.indexOf(value))
+            returnedArr.push(arr[i])
+            console.log(returnedArr)
+        }
+
+    }
+    // return console.log(returnedArr)
+}
+
+linearSearch(11)
+
+// 6.42
