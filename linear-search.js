9a9c0795415bb2ddf215d3837ad1b31d93c04966
Amelit <muradovt111@gmail.com>
2021-09-06 21:37:42 +0300

    refactor for linear-search, added counter and added description for values which function returns

--- linear-search.js
+++ linear-search.js
@@ -1,19 +1,16 @@
 ﻿const arr = [3,1,2,6,7,9,7,11,34,567,77,555,33,223]
-//TODO: refactor this function
-
+let count = 0;
 function linearSearch(value) {
     for (let i = 0; i <= arr.length; i++) {
-        let returnedArr = []
+        count += 1
         if (arr[i] === value) {
-            console.log(arr.indexOf(value))
-            returnedArr.push(arr[i])
-            console.log(returnedArr)
+            console.log(count, "this is the counter of iteration times")
+            console.log(arr.indexOf(value), "this is the index of the element which was forwarded to function as an argument")
+            return console.log(value, "that is the value which was founded by the cycle")
         }
 
     }
-    // return console.log(returnedArr)
+    return console.log("Your value is not found inside of array")
 }
 
 linearSearch(11)
-
-// 6.42
