26d720b00144bf6c848c82ae9220e66365b5dbe5
Amelit <muradovt111@gmail.com>
2021-09-08 20:11:16 +0300

    fix for binary-search

--- binary-search.js
+++ binary-search.js
@@ -1,33 +1,34 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
-let count
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
+
+let count = 0;
 let firstElement = 0;
-let lastElement = arr.length
-let middleElement = Math.floor((firstElement + lastElement) / 2)
-let found = false
+let lastElement = arr.length;
+let middleElement;
+let found = false;
 let position = -1;
 
-//TODO: Deal what's wrong with function
-
-function binarySearch(value) {
-
+const binarySearch = (array, value) => {
     while (found === false && firstElement <= lastElement) {
         count += 1
-        if (arr[middleElement] === value) {
+        middleElement = Math.floor((firstElement + lastElement) / 2);
+        if (array[middleElement] === value) {
             found = true
             position = middleElement
-            return position
+            return position;
         }
 
-        if (value < arr[middleElement]) {
+        if (value < array[middleElement]) {
             lastElement = middleElement - 1
         } else {
             firstElement = middleElement + 1
         }
 
     }
-    console.log(count)
-    return position
+    return position;
 
 }
 
-binarySearch(23);
+console.log(binarySearch(arr, 23), count);
+
+
+
